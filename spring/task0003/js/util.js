ffd694c484424518b42e91a2a7268657b9f2e2c4
Yim <lymwnwh@qq.com>
2017-07-09 11:06:54 +0800

    some changes

--- spring/task0003/js/util.js
+++ spring/task0003/js/util.js
@@ -287,49 +287,76 @@ function getPosition(element) {
 //task3.2
 //实现一个简单的query
 function $(selector) {
-    var ele = document;
+    var allchilds = [];
+    var childs = function (element) {    // 递归获取所有子元素
+        return element.getElementsByTagName('*');
+    }
+
+    var ele = document.getElementsByTagName('html')[0];    // 获取所有元素
     var sele = selector.replace(/\s+/, ' ').split(' ');    // 去除多余的空格并分割
 
     for (var i = 0, len = sele.length; i < len; i++) {
+        ele = childs(ele);
+        var eleLen = ele.length;
+        var isGet = false;
 
         switch (sele[i][0]) {    // 从子节点中查找
             case '#':
-                ele = ele.getElementById(sele[i].substring(1));
+                for (var j = 0; j < eleLen; j++) {
+                    if (ele[j].id === sele[i].substring(1)) {
+                        ele = ele[j];
+                        isGet = true;
+                        break;
+                    }
+                }
                 break;
             case '.':
-                ele = ele.getElementsByClassName(sele[i].substring(1))[0];
+                for (var j = 0; j < eleLen; j++) {
+                    var name = uniqArray(ele[j].className.split(' '));
+                    if (name.indexOf(sele[i].substring(1)) !== -1) {
+                        ele = ele[j];
+                        isGet = true;
+                        break;
+                    }
+                }
                 break;
             case '[':
                 var valueLoc = sele[i].indexOf('=');
-                var temp = ele.getElementsByTagName('*');
-                var tLen = temp.length;
                 if (valueLoc !== -1) {
                     var key = sele[i].substring(1, valueLoc);
                     var value = sele[i].substring(valueLoc + 1, sele[i].length - 1);
-                    for (var j = 0; j < tLen; j++) {
-                        if (temp[j][key] === value) {
-                            ele = temp[j];
+                    for (var j = 0; j < eleLen; j++) {
+                        if (ele[j][key] === value) {
+                            ele = ele[j];
+                            isGet = true;
                             break;
                         }
                     }
                 }
                 else {
                     var key = sele[i].substring(1, sele[i].length - 1);
-                    for (var j = 0; j < tLen; j++) {
-                        if (temp[j][key]) {
-                            ele = temp[j];
+                    for (var j = 0; j < eleLen; j++) {
+                        if (ele[j][key]) {
+                            ele = ele[j];
+                            isGet = true;
                             break;
                         }
                     }
                 }
                 break;
             default :
-                ele = ele.getElementsByTagName(sele[i])[0];
+                for (var j = 0; j < eleLen; j++) {
+                    if (ele[j].tagName === sele[i].toUpperCase()) {    // tagName 属性的返回值始终是大写的
+                        ele = ele[j];
+                        isGet = true;
+                        break;
+                    }
+                }
                 break;
         }
     }
 
-    if (!ele) {
+    if (!isGet) {
         ele = null;
     }
 
